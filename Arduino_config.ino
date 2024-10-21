40d7959d260050baa26367489a1be0e7856b12c5
Ondřej Homolka <Ondys.oh@gmail.com>
2024-10-21 23:56:28 +0200

    changing sensor values to variables

--- Arduino_config.ino
+++ Arduino_config.ino
@@ -3,18 +3,18 @@
 #include <HTTPClient.h>
 #include "Matrix2D.h"
 
-const char* ssid = "Ondys-iPhone";
-const char* password = "12345678";
+#define CLK_PIN 13
+#define LAT_PIN 12
+#define DATA_PIN 11
 
 using namespace tinyxml2;
+
+const char* ssid = "Ondys-iPhone";
+const char* password = "12345678";
 
 Matrix2D Matrix;
 XMLDocument doc;
 HTTPClient http;
-
-#define CLK_PIN 13
-#define LAT_PIN 12
-#define DATA_PIN 11
 
 void setup() {
 
@@ -30,14 +30,21 @@ void setup() {
 
 
 void loop() {
+  
   String RAWxml;
+  String value_dir;
+  String value_speed;
+
   delay(1000);
+
   if (WiFi.status() == WL_CONNECTED) {                                //checks if WIFI is connected
     http.begin("http://46.13.10.244:8005/xml.xml");                   //connects to meteostation
     int statusCode = http.GET();                                      //gets status code from meteostation
 
-    if (statusCode > 0) {                                             //checks if HTTP is comunicating
-      String RAWxml = http.getString();                               //gets XML data to string
+    if (statusCode > 0) 
+    {                                             //checks if HTTP is comunicating
+      //String type;
+      RAWxml = http.getString();                                      //gets XML data to string
       Serial.println("\nStatus:" + String(statusCode));               //prints status code 
       //Serial.println(RAWxml);
 
@@ -47,17 +54,22 @@ void loop() {
       //Serial.println("2");
       for (tinyxml2::XMLElement* child = SensorList->FirstChildElement(); child != NULL; child = child->NextSiblingElement())     // reads every sensor element of XML
       {
-        String type = child->FirstChildElement("type")->GetText();        //navigates to type in every sensor
-        if(type == "wind_direction" || type == "wind_speed")              //filters by type
+        String type = child->FirstChildElement("type")->GetText();             //navigates to type in every sensor and saves their data
+        if(type == "wind_direction")                   //filters by type
         {
-          Serial.println(child->FirstChildElement("value")->GetText());   //gets value from sensors and prints it
+          value_dir = child->FirstChildElement("value")->GetText();  //gets value from sensors and prints them
+          Serial.println(value_dir.toInt());                                //converts data to Int
+        }
+        if(type == "wind_speed")                   //filters by type
+        {
+          value_speed = child->FirstChildElement("value")->GetText();  //gets value from sensors and prints them
+          Serial.println(value_speed.toInt());                                //converts data to Int
         }
       }
     } 
     else {
       Serial.println("Http error");                 //HTTP connection lost
     }
-
   } 
   else {
     Serial.println("Connection lost");
